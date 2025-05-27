c246208d2b1b5db4e5415d39c2828ecb536ef69e
focandlol <focandlol@naver.com>
2025-05-27 19:06:40 +0900

    feat:
    mysql, spring data jpa 의존성 추가
    Report 엔티티 생성

---
+++ src/main/java/focandlol/calamity/domain/Report.java
@@ -0,0 +1,35 @@
+package focandlol.calamity.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Report extends BaseEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String title;
+
+  @Column(columnDefinition = "TEXT")
+  private String content;
+
+  private String roadAddress;
+
+  private String jibunAddress;
+
+  private String category;
+}
+
